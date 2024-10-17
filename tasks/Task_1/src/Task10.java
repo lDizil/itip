33d1e15c08a32739c1664a17a5693af1e7ba7e21
Denis <plastinin.denis2009@mail.ru>
2024-10-17 10:45:17 +0300

    removing unuse files

--- tasks/Task_1/src/Task10.java
+++
@@ -1,9 +0,0 @@
-public class Task10 {
-    public static void main(String[] args) {
-        int students = 53;
-        int tables = 58;
-        int requiredTables = (int) Math.ceil(students / 2.0);
-        int result = (requiredTables > tables) ? (requiredTables - tables) : 0;
-        System.out.println(result);
-    }
-}
--- tasks/Task_1/src/Task9.java
+++
@@ -1,8 +0,0 @@
-public class Task9 {
-    public static void main(String[] args) {
-        int ticket = 45;
-        int cost = 950;
-        float ticketSaler = (ticket * cost) * 0.72f;
-        System.out.println(ticketSaler);
-    }
-}
